d719d0c6a0b5e34e8472a55d26c8453586d19de4
Christopher Fonseka <chris.fonseka@googlemail.com>
2019-11-27 23:48:47 +0000

    Initial Commit for the WeSplit project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Christopher Fonseka on 27/11/2019.
+//  Copyright © 2019 Christopher Fonseka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
