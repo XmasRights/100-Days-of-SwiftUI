72dcebcdf7a895dda091a7d2d56559e14ddc7e55
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-04-27 21:06:23 +0100

    Added a date picker to Better Sleep

--- Better Rest/BetterRest/BetterRest/ContentView.swift
+++ Better Rest/BetterRest/BetterRest/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
 
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...24, step: 0.25) {
-            Text("\(sleepAmount) hours")
+        VStack(spacing: 20) {
+            Stepper(value: $sleepAmount, in: 4...24, step: 0.25) {
+                Text("\(sleepAmount, specifier: "%g") hours")
+            }
+
+            DatePicker("Please Pick a Date",
+                       selection: $wakeUp,
+                       in: Date()...)
+                .labelsHidden()
         }
     }
 }
